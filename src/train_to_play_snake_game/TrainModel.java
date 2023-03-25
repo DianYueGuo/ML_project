66799ff651d8c4e2c22fef5d150ebcbf4b63ed12
郭典約 <dianyueguo@gmail.com>
2023-03-25 20:37:40 +0800

    add AverageScoreBoard

--- src/train_to_play_snake_game/TrainModel.java
+++ src/train_to_play_snake_game/TrainModel.java
@@ -8,10 +8,53 @@ import train_to_play_snake_game.SnakeGame.HeadDirection;
 
 public class TrainModel {
 
+	static class AverageScoreBoard {
+
+		private int totalNumberOfSteps;
+		private int numberOfApplesEaten;
+		private int numberOfGames;
+
+		AverageScoreBoard() {
+			totalNumberOfSteps = 0;
+			numberOfApplesEaten = 0;
+			numberOfGames = 0;
+		}
+
+		void addGame(SnakeGame game) {
+			numberOfGames++;
+			totalNumberOfSteps += game.getScoreBoard().getTotalNumberOfSteps();
+			numberOfApplesEaten += game.getScoreBoard().getNumberOfApplesEaten();
+		}
+
+		float getNumberOfStepsPerGame() {
+			return totalNumberOfSteps / (float) numberOfGames;
+		}
+
+		float getNumberOfApplesEatenPerGame() {
+			return numberOfApplesEaten / (float) numberOfGames;
+		}
+
+		float getStepsPerApplePerGame() {
+			return totalNumberOfSteps / (float) numberOfApplesEaten / numberOfGames;
+		}
+
+		@Override
+		public String toString() {
+			StringBuilder sb = new StringBuilder();
+			sb.append("NumberOfApplesEatenPerGame: " + getNumberOfApplesEatenPerGame() + "\n");
+			sb.append("StepsPerApplePerGame: " + getStepsPerApplePerGame() + "\n");
+			sb.append("NumberOfStepsPerGame: " + getNumberOfStepsPerGame() + "\n");
+
+			return sb.toString();
+		}
+
+	}
+
 	public static void train() throws Exception {
 		NeuralNetwork model1 = new NeuralNetwork(
-				new int[] { SnakeGame.FIELD_WIDTH * SnakeGame.FIELD_HEIGHT, 32, 32, 2 }, new ActivationFunction[] {
-						ActivationFunction.SIGMOID, ActivationFunction.SIGMOID, ActivationFunction.STEP });
+				new int[] { SnakeGame.FIELD_WIDTH * SnakeGame.FIELD_HEIGHT, 16, 16, 16, 2 },
+				new ActivationFunction[] { ActivationFunction.SIGMOID, ActivationFunction.SIGMOID,
+						ActivationFunction.SIGMOID, ActivationFunction.STEP });
 
 		model1.mutate(1);
 
@@ -23,44 +66,47 @@ public class TrainModel {
 			variants[0] = model1;
 
 			for (int i = 1; i < variants.length; i++) {
-				variants[i] = model1.clone();
-				variants[i].mutate((float) 0.008);
+				variants[i] = variants[i - 1].clone();
+				variants[i].mutate((float) 0.0001);
 			}
 
-			SnakeGame[] games = new SnakeGame[variants.length];
+			AverageScoreBoard[] avgScoreBoards = new AverageScoreBoard[variants.length];
 
 			for (int i = 0; i < variants.length; i++) {
-				games[i] = new SnakeGame();
+				avgScoreBoards[i] = new AverageScoreBoard();
 
-				play(games[i], variants[i]);
+				for (int t = 0; t < SnakeGame.FIELD_WIDTH * SnakeGame.FIELD_HEIGHT * 4; t++) {
+					SnakeGame game = new SnakeGame();
+
+					play(game, variants[i]);
+
+					avgScoreBoards[i].addGame(game);
+				}
 			}
 
 			int selectionIndex = 0;
 			for (int i = 0; i < variants.length; i++) {
-				if (games[selectionIndex].getScoreBoard().getStepsPerApple() > games[i].getScoreBoard()
-						.getStepsPerApple()) {
+				if (avgScoreBoards[selectionIndex].getNumberOfApplesEatenPerGame() < avgScoreBoards[i]
+						.getNumberOfApplesEatenPerGame()) {
 					selectionIndex = i;
-				} else if (games[selectionIndex].getScoreBoard().getStepsPerApple() == games[i].getScoreBoard()
-						.getStepsPerApple()) {
-					if (games[selectionIndex].getScoreBoard().getTotalNumberOfSteps() < games[i].getScoreBoard()
-							.getTotalNumberOfSteps()) {
-						selectionIndex = i;
-					} else if (games[selectionIndex].getScoreBoard().getTotalNumberOfSteps() == games[i].getScoreBoard()
-							.getTotalNumberOfSteps()) {
-						if (games[selectionIndex].getScoreBoard().getNumberOfApplesEaten() < games[i].getScoreBoard()
-								.getNumberOfApplesEaten()) {
-							selectionIndex = i;
-						} else if (games[selectionIndex].getScoreBoard().getNumberOfApplesEaten() == games[i]
-								.getScoreBoard().getNumberOfApplesEaten()) {
-
-						}
-					}
+				} else if (avgScoreBoards[selectionIndex].getNumberOfApplesEatenPerGame() == avgScoreBoards[i]
+						.getNumberOfApplesEatenPerGame()) {
+//					if (avgScoreBoards[selectionIndex].getStepsPerApplePerGame() > avgScoreBoards[i].getStepsPerApplePerGame()) {
+//						selectionIndex = i;
+//					} else if (avgScoreBoards[selectionIndex].getStepsPerApplePerGame() == avgScoreBoards[i].getStepsPerApplePerGame()) {
+//						
+//					}
 				}
+//				if (avgScoreBoards[selectionIndex].getNumberOfStepsPerGame() < avgScoreBoards[i].getNumberOfStepsPerGame()) {
+//					selectionIndex = i;
+//				} else if (avgScoreBoards[selectionIndex].getNumberOfStepsPerGame() == avgScoreBoards[i].getNumberOfStepsPerGame()) {
+//					
+//				}
 			}
 
 			model1 = variants[selectionIndex];
 
-			System.out.println(games[selectionIndex].getScoreBoard());
+			System.out.println(avgScoreBoards[selectionIndex]);
 		}
 	}
 
