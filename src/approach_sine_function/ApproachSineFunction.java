8045e8007a93a046c74e1b07cb88c1a9cf5222a8
郭典約 <dianyueguo@gmail.com>
2023-04-10 10:34:25 +0800

    make revision

--- src/approach_sine_function/ApproachSineFunction.java
+++ src/approach_sine_function/ApproachSineFunction.java
@@ -1,7 +1,7 @@
 package approach_sine_function;
 
-import org.json.JSONException;
-import org.json.JSONObject;
+import java.io.PrintWriter;
+import java.util.Date;
 
 import machine_learning.ActivationFunction;
 import machine_learning.DoubleMatrix;
@@ -10,53 +10,73 @@ import machine_learning.NeuralNetwork;
 public class ApproachSineFunction {
 
 	public static void train() throws Exception {
-		NeuralNetwork model = new NeuralNetwork(new int[] { 1, 32, 32, 1 }, new ActivationFunction[] {
-				ActivationFunction.SIGMOID, ActivationFunction.SIGMOID, ActivationFunction.LINEAR });
+		PrintWriter writer = new PrintWriter("/Users/Joseph/Desktop/" + new Date() + ".csv", "UTF-8");
+		System.out.println("create file: \"/Users/Joseph/Desktop/" + new Date() + ".csv\"");
+
+		int number_of_test_points = 100;
+
+		writer.print("generation,");
+		for (int testPoint = 0; testPoint < number_of_test_points; testPoint++) {
+			double x = 2 * Math.PI * testPoint / number_of_test_points;
+
+			writer.print(x + ",");
+		}
+		writer.print("fitness\n");
+
+		NeuralNetwork model = new NeuralNetwork(new int[] { 1, 16, 16, 16, 1 },
+				new ActivationFunction[] { ActivationFunction.SIGMOID, ActivationFunction.SIGMOID,
+						ActivationFunction.SIGMOID, ActivationFunction.LINEAR });
 
 		model.mutate(1);
 
-		System.out.println(model);
+		double maxFitness = 0;
 
-		double minSsd = 1000;
+		for (int generation = 0; generation <= 200; generation++) {
 
-		for (int generation = 0; generation < 2000 && minSsd >= 0.01; generation++) {
+			writer.print(generation + ",");
+			double sum_of_error_square = 0;
+			for (int testPoint = 0; testPoint < number_of_test_points; testPoint++) {
+				double x = 2 * Math.PI * testPoint / number_of_test_points;
+
+				double predicted_value = model.getOutput(new DoubleMatrix(new double[][] { { x } }).transpose()).get(0,
+						0);
+				writer.print(predicted_value + ",");
+
+				sum_of_error_square += Math.pow(predicted_value - Math.sin(x), 2);
+			}
+			double fitness = number_of_test_points / sum_of_error_square;
+			writer.print(fitness + "\n");
+			
+			System.out.println("generation: " + generation + ", fitness: " + fitness);
 
 			NeuralNetwork selectedVariant = model;
 
-			for (int variant_index = 0; variant_index < 500; variant_index++) {
+			for (int variant_index = 0; variant_index < 1000; variant_index++) {
 				NeuralNetwork variant = model.clone();
-				variant.mutate(0.005f);
+				variant.mutate(0.01);
 
-				double ssd = 0;
-				for (int testPoint = 0; testPoint < 100; testPoint++) {
-					double x = 2 * Math.PI * testPoint / 100;
+				sum_of_error_square = 0;
+				for (int testPoint = 0; testPoint < number_of_test_points; testPoint++) {
+					double x = 2 * Math.PI * testPoint / number_of_test_points;
 
-					ssd += Math.pow(variant.getOutput(new DoubleMatrix(new double[][] { { x } }).transpose()).get(0, 0)
-							- Math.sin(x), 2);
+					double predicted_value = variant.getOutput(new DoubleMatrix(new double[][] { { x } }).transpose())
+							.get(0, 0);
+
+					sum_of_error_square += Math.pow(predicted_value - Math.sin(x), 2);
 				}
 
-				if (ssd < minSsd) {
-					minSsd = ssd;
+				fitness = number_of_test_points / sum_of_error_square;
+				
+				if (fitness > maxFitness) {
+					maxFitness = fitness;
 					selectedVariant = variant;
 				}
 			}
 
 			model = selectedVariant;
-
-			System.out.println("minSsd=" + minSsd);
-
 		}
 
-		System.out.println(model);
-	}
-
-	public static void test(String str) throws JSONException, Exception {
-		NeuralNetwork model = new NeuralNetwork(new JSONObject(str));
-
-		for (int testPoint = 0; testPoint < 100; testPoint++) {
-			double x = 2 * Math.PI * testPoint / 100;
-			System.out.println(model.getOutput(new DoubleMatrix(new double[][] { { x } }).transpose()).get(0, 0));
-		}
+		writer.close();
 	}
 
 }
